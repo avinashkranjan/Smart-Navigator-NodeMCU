8c5598c1ad3d4d846db8164b10dc8c5832625d8c
avinashkranjan <ranjan.avinash@hotmail.com>
2022-02-27 02:24:49 +0530

    app initialized

---
+++ node_switch/android/app/src/main/kotlin/com/avinashkranjan/nodeswitch/node_switch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.avinashkranjan.nodeswitch.node_switch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ node_switch/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'components/NodeMCU_Switch.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Node Switch',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NodeMCU_Switch(),
+    );
+  }
+}
